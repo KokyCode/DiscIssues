e8c43a83c1b070cae906f521a129314c6f0e8ab3
Kokydev <osocoding@gmail.com>
2020-09-23 23:55:38 +0100

    Working release

--- main.js
+++ main.js
@@ -8,41 +8,47 @@ const {
 const { Octokit } = require("@octokit/rest");
 const differenceBy = require('lodash/differenceBy')
 
+
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 // Call GitHub's API
 const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN
+    auth: "GITAUTH"
 });
 
 
 
 function queryGitHub(keyword, delay) {
-    console.log('Checking for keyword:', keyword)
-    let collectedIssues = []
-    setInterval(() => {
-        octokit.search.issuesAndPullRequests({
-            q: `${keyword.replace(" ", "+")}+is:issue+state:open+NOT+Acunetix+NOT+syhunt+NOT+bump+NOT+upgrade+NOT+fix`,
-            sort: 'created',
-            order: 'desc',
-            per_page: 100
-        }).then((response) => {
+  console.log('Checking for keyword:', keyword)
+  let collectedIssues = []
+  setInterval(() => {
+          octokit.search.issuesAndPullRequests({
+              q: `${keyword.replace(" ", "+")}+is:issue+state:open+NOT+Acunetix+NOT+syhunt+NOT+bump+NOT+upgrade+NOT+fix`,
+              sort: 'created',
+              order: 'desc',
+              per_page: 100
+          })
+
+          .then((response) => {
             const latestCollection = response.data.items
             if (collectedIssues.length === 0) collectedIssues = latestCollection
 
             const newIssues = differenceBy(latestCollection, collectedIssues, 'id')
-            if (newIssues.length) {
-                client.channels.cache.get('466723055532638247').send(`[${keyword}] New issue found:`, newIssues.map((issue) => {
+            if (newIssues.length >= 1) {
+                const mapIssue = newIssues.map((issue) => {
                     return issue.html_url
-                }))
+                }).pop()
+                console.log(mapIssue)
+                client.channels.cache.get('466723055532638247').send(`New issue (${keyword}) found:\n ${mapIssue}`)
             }
 
             collectedIssues = collectedIssues.concat(newIssues)
-        })
-    }, delay)
+          })
+  }, delay)
 }
-
+//const keywords = ['xss vulnerable', 'injection vulnerable', 'denial vulnerable', 'traversal vulnerable'] // MAX 20
 const keywords = ['error', 'issue'] // MAX 20
 const delayLength = 60000 / 20 * keywords.length // 1 minute rate limit / 20 requests per minute * amount of keywords
 if (isMainThread) {
@@ -66,7 +72,7 @@ if (isMainThread) {
     queryGitHub(workerData.keyword, workerData.delay)
 }
 
-client.login('NzU4NDMwODMxOTA2NjUyMTYw.X2u1wQ.8z1QBUmazYTTU18AbZtBMhZ7i0o');       
+client.login('TOKEN');       
     
 
 

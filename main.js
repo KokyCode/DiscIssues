4100b0f7db0dea69a4a8af967d73d4160b70d85f
Kokydev <osocoding@gmail.com>
2020-09-23 22:37:04 +0100

    Main

--- main.js
+++ main.js
@@ -8,10 +8,15 @@ const {
 const { Octokit } = require("@octokit/rest");
 const differenceBy = require('lodash/differenceBy')
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
 // Call GitHub's API
 const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN
 });
+
+
 
 function queryGitHub(keyword, delay) {
     console.log('Checking for keyword:', keyword)
@@ -28,7 +33,7 @@ function queryGitHub(keyword, delay) {
 
             const newIssues = differenceBy(latestCollection, collectedIssues, 'id')
             if (newIssues.length) {
-                console.log(`[${keyword}] New issue found:`, newIssues.map((issue) => {
+                client.channels.cache.get('466723055532638247').send(`[${keyword}] New issue found:`, newIssues.map((issue) => {
                     return issue.html_url
                 }))
             }
@@ -38,10 +43,16 @@ function queryGitHub(keyword, delay) {
     }, delay)
 }
 
-const keywords = ['xss vulnerable', 'injection vulnerable', 'denial vulnerable', 'traversal vulnerable'] // MAX 20
+const keywords = ['error', 'issue'] // MAX 20
 const delayLength = 60000 / 20 * keywords.length // 1 minute rate limit / 20 requests per minute * amount of keywords
 if (isMainThread) {
-    console.log('Main: Starting the main thread.')
+    console.log('Main: Starting the main thread. Connecting to the bot...')
+
+    client.on('ready', () => {
+      console.log(`Connected! Logged in as ${client.user.tag}!`);
+      client.channels.cache.get('466723055532638247').send('Initiated. Version 1.0.0 - Developed by k0rrupt#0394 & Koky#1337')
+    });
+
     for (var i = 0; i < keywords.length; i++) {
         // Create a new worker, referencing own filename
         new Worker(__filename, {
@@ -54,9 +65,8 @@ if (isMainThread) {
 } else { // Otherwise, we are on a worker thread, do work
     queryGitHub(workerData.keyword, workerData.delay)
 }
-          
-           
-                          
 
+client.login('NzU4NDMwODMxOTA2NjUyMTYw.X2u1wQ.8z1QBUmazYTTU18AbZtBMhZ7i0o');       
+    
 
 
